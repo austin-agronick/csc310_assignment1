e7de1b0e01d4f7d71416cb660e4236ee58b78f90
root <root@CSF102.localdomain>
2020-02-05 18:35:50 -0500

    done

--- main.py
+++ main.py
@@ -7,53 +7,54 @@ max_generations = 5
 
 def generationCalculation(grid, rows, cols):
     liveNeighbors = 0
-    nextGenGrid = grid.buffer
-    
+
+    nextGenGrid = grid
+
     for i in range(cols):
 
         for j in range(rows):
 
             if(i+1 <= cols-1):
-                if(grid.buffer[i+1][j] == 1):
+                if(grid[i+1][j] == 1):
                     liveNeighbors += 1
             if(i-1 >= 0):
-                if(grid.buffer[i-1][j] == 1):
+                if(grid[i-1][j] == 1):
                     liveNeighbors += 1
             if(j+1 <= rows-1):
-                if(grid.buffer[i][j+1] == 1):
+                if(grid[i][j+1] == 1):
                     liveNeighbors += 1
             if(j-1 >= 0):
-                if(grid.buffer[i][j-1] == 1):
+                if(grid[i][j-1] == 1):
                     liveNeighbors += 1
                 #diagonals
             if(i+1 <= cols-1 and j+1 <= rows-1):
-                if(grid.buffer[i+1][j+1] == 1):
+                if(grid[i+1][j+1] == 1):
                     liveNeighbors += 1
             if(i-1 >= 0 and j-1 >= 0):
-                if(grid.buffer[i-1][j-1] == 1):
+                if(grid[i-1][j-1] == 1):
                     liveNeighbors += 1
             if(i-1 >= 0 and j+1 <= rows-1):
-                if(grid.buffer[i-1][j+1] == 1):
+                if(grid[i-1][j+1] == 1):
                     liveNeighbors += 1
             if(i+1 <= cols-1 and j-1 >= 0):
-                if(grid.buffer[i+1][j-1] == 1):
+                if(grid[i+1][j-1] == 1):
                     liveNeighbors += 1
 
-        liveNeighbors -= grid.buffer[i][j]
+            liveNeighbors -= grid[i][j]
 
-        if grid.buffer[i][j] == 1 and liveNeighbors < 2:
-            nextGenGrid[i][j] = 0
-        
-        elif grid.buffer[i][j] == 1 and liveNeighbors > 3:
-            nextGenGrid[i][j] = 0
+            if grid[i][j] == 1 and liveNeighbors < 2:
+                nextGenGrid[i][j] = 0
+            
+            elif grid[i][j] == 1 and liveNeighbors > 3:
+                nextGenGrid[i][j] = 0
 
-        elif grid.buffer[i][j] == 1 and liveNeighbors == 3:
-            nextGenGrid[i][j] = 1
+            elif grid[i][j] == 1 and liveNeighbors == 3:
+                nextGenGrid[i][j] = 1
 
-        else:
-            nextGenGrid[i][j] = grid.buffer[i][j]
+            else:
+                nextGenGrid[i][j] = grid[i][j]
 
-        return nextGenGrid
+    return nextGenGrid
 
 
 def main():
@@ -64,6 +65,6 @@ def main():
     while grid.generations < max_generations:
         grid.display()
         grid.generations += 1
-        grid.buffer = generationCalculation(grid, width, height)
+        grid.buffer = generationCalculation(grid.buffer, width, height)
 
 main()
